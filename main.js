246ef195203284cd08be0e8fe818da5a161ff804
Mohamed Tobal <113104800+Mohamed-Hamdy-Tobal@users.noreply.github.com>
2023-03-14 11:52:34 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 let sectionSkill = document.querySelector(".our-skills")
 let mySpan = document.querySelectorAll(".our-skills .skills .prog span")
+
 let sectionState = document.querySelector(".state")
 let num = document.querySelectorAll(".state .container .box h2")
 let started = false
 
 window.onscroll = function () {
+    // For Progress Skills
     if (window.scrollY >= sectionSkill.offsetTop -300) {
         mySpan.forEach((span) => {
             span.style.width = span.dataset.width
         })
     }
+    // For State
     if (window.scrollY >= sectionState.offsetTop - 300) {
-    if (!started) {
-        num.forEach(item => startCount(item))
-    }
-    started = true
+        if (!started) {
+            num.forEach(item => startCount(item))
+        }
+        started = true
     }
 }
 
@@ -27,4 +30,3 @@ function startCount(el) {
         }
     },(2000 / goal))
 }
-
